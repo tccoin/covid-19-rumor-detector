6424a0a3c6e0e74fe460cc9fb20c6b77e739104a
Tccoin <1774241372@qq.com>
2020-06-15 00:35:17 +0800

    爬取谷歌搜索结果作为谣言数据集

---
+++ merge_google_results.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+(async () => {
+    let output = [
+        ['keyword','title','summary','link','visible_link','date','rank']
+    ];
+    let writeStream = fs.createWriteStream('data/google.csv');
+    for(let i=0;i<28;i++){
+        obj = JSON.parse(fs.readFileSync('data/google_results/'+i+'.json', 'utf8'));
+        for(let keyword of Object.keys(obj)){
+            for(let result of obj[keyword]["1"]["results"]){
+                output.push([
+                    keyword,
+                    result['title'].replace(/,/g,'，'),
+                    result['snippet'].replace(/,/g,'，'),
+                    result['link'].replace(/,/g,'，'),
+                    result['visible_link'].replace(/,/g,'，'),
+                    result['date'],
+                    result['rank']
+                ]);
+            }
+        }
+    }
+    for(let row of output){
+        writeStream.write(row.join(',')+ '\n');
+    }
+    writeStream.end();
+    
+})();
