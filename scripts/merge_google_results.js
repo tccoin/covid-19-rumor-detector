59f41d2cc3ae2f1d55549c24d0858b8b268e3542
Link Wu <1774241372@qq.com>
2020-06-16 22:24:32 +0800

    udpate google_rumors.csv

--- scripts/merge_google_results.js
+++ scripts/merge_google_results.js
@@ -1,28 +1,28 @@
 const fs = require('fs');
 (async () => {
     let output = [
-        ['keyword','title','summary','link','visible_link','date','rank']
+        ['keyword', 'title', 'summary', 'link', 'visible_link', 'date', 'rank']
     ];
-    let writeStream = fs.createWriteStream('data/google.csv');
-    for(let i=0;i<28;i++){
-        obj = JSON.parse(fs.readFileSync('data/google_results/'+i+'.json', 'utf8'));
-        for(let keyword of Object.keys(obj)){
-            for(let result of obj[keyword]["1"]["results"]){
+    let writeStream = fs.createWriteStream('../data/Google_rumors.csv');
+    for (let i = 0; i < 28; i++) {
+        obj = JSON.parse(fs.readFileSync('../data/google_results/' + i + '.json', 'utf8'));
+        for (let keyword of Object.keys(obj)) {
+            for (let result of obj[keyword]["1"]["results"]) {
                 output.push([
                     keyword,
-                    result['title'].replace(/,/g,'，'),
-                    result['snippet'].replace(/,/g,'，'),
-                    result['link'].replace(/,/g,'，'),
-                    result['visible_link'].replace(/,/g,'，'),
+                    result['title'].replace(/,/g, '，'),
+                    result['snippet'].replace(/,/g, '，').replace(/\n/g, '。'),
+                    result['link'].replace(/,/g, '，'),
+                    result['visible_link'].replace(/,/g, '，'),
                     result['date'],
                     result['rank']
                 ]);
             }
         }
     }
-    for(let row of output){
-        writeStream.write(row.join(',')+ '\n');
+    for (let row of output) {
+        writeStream.write(row.join(',') + '\n');
     }
     writeStream.end();
-    
+
 })();
