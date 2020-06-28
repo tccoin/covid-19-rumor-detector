191a9d579062a6a5c9e9af5eeb123241e61a0aa8
Tccoin <1774241372@qq.com>
2020-06-29 05:45:32 +0800

    更新谣言检测脚本和相关性分析脚本

--- scripts/merge_google_results.js
+++ scripts/merge_google_results.js
@@ -3,21 +3,27 @@ const fs = require('fs');
     let output = [
         ['keyword', 'title', 'summary', 'link', 'visible_link', 'date', 'rank']
     ];
-    let writeStream = fs.createWriteStream('../data/Google_rumors.csv');
+    let writeStream = fs.createWriteStream('./data/Google_rumors.csv');
+    keyword_dict = {'南京封城、封路':[]}
     for (let i = 0; i < 28; i++) {
-        obj = JSON.parse(fs.readFileSync('../data/google_results/' + i + '.json', 'utf8'));
+        obj = JSON.parse(fs.readFileSync('./data/google_results/' + i + '.json', 'utf8'));
         for (let keyword of Object.keys(obj)) {
-            for (let result of obj[keyword]["1"]["results"]) {
-                output.push([
-                    keyword,
-                    result['title'].replace(/,/g, '，'),
-                    result['snippet'].replace(/,/g, '，').replace(/\n/g, '。'),
-                    result['link'].replace(/,/g, '，'),
-                    result['visible_link'].replace(/,/g, '，'),
-                    result['date'],
-                    result['rank']
-                ]);
-            }
+            // if('南京封城、封路' in keyword_dict){console.log('!!!')}
+            // if(keyword=='南京封城、封路'){console.log('!!!')}
+            keyword_dict[keyword] = obj[keyword]["1"]["results"];
+        }
+    }
+    for (let keyword of Object.keys(keyword_dict)) {
+        for(let result of keyword_dict[keyword]){
+            output.push([
+                keyword,
+                result['title'].replace(/,/g, '，'),
+                result['snippet'].replace(/,/g, '，').replace(/\n/g, '。'),
+                result['link'].replace(/,/g, '，'),
+                result['visible_link'].replace(/,/g, '，'),
+                result['date'],
+                result['rank']
+            ]);
         }
     }
     for (let row of output) {
